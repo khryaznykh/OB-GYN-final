5fb82b2c9f2fe6960bfbc1d9fcd7eb7b30a6352d
khryaznykh <khryaznykh@gmail.com>
2024-07-14 14:28:05 -0400

    adding animation

--- src/whattoexpect/Obgyn.js
+++ src/whattoexpect/Obgyn.js
@@ -1,17 +1,9 @@
 import { useNavigate } from "react-router";
 import { useState } from "react";
 import { ObgynData } from "./Obgyn-data";
-import gsap from 'gsap';
-import { useGSAP } from "@gsap/react";
+import { motion } from "framer-motion";
 
 function Obgyn () {
-    useGSAP(
-        ()=> {
-            gsap.from ('.rectangle-container', { y:-200, duration:1, opacity:0})
-            gsap.from ('.text-container', { y:-200, delay: 0.3, duration:1, opacity:0})
-        }
-    )
-
     const navigate = useNavigate();
     const [visit,setVisit] = useState(0);
     const {id, image, title, text, texttwo, textthree, textfour, textfive, textsix, textseven} = ObgynData[visit];
@@ -46,15 +38,24 @@ const visitNext = () => {
                 </div>
             <div>
                 <div className="image-text">
-                    <div className="rectangle-container">
-                        <div>
+                    <motion.div className="rectangle-container"
+                      initial={{y:-70}}
+                      whileInView={{y:0, transition: {duration:0.7, staggerChildren: 1}}}
+                      viewport={{once:true}}>
+                        <motion.div
+                          initial={{y:-70, opacity:0}}
+                          whileInView={{y:0, opacity:1, transition: {duration:0.7}}}
+                          viewport={{once:true}}>
                             <img className="ob-image" src={image} width={450} alt="OB/GYN"/>
                             <div className="rectangle"></div>
-                                <div className="rectangle-two"></div>
-                            </div>
+                            <div className="rectangle-two"></div>
+                        </motion.div>
                             <button  onClick={()=> navigate(-1)} className="btn-goback"><span>Go Back</span></button> 
-                        </div>
-                    <div className="text-container">
+                    </motion.div>
+                    <motion.div className="text-container"
+                     initial={{y:-70, opacity: 0}}
+                     whileInView={{y:0, opacity: 1, transition: {duration:0.7}}}
+                     viewport={{once:true}}>
                         <p className="p-text">{text}</p>
                         <p className="p-text">{texttwo}</p>
                         <p className="p-text">{textthree}</p>
@@ -62,7 +63,7 @@ const visitNext = () => {
                         <p className="p-text">{textfive}</p>
                         <p className="p-text">{textsix}</p>
                         <p className="p-text">{textseven}</p>
-                    </div>
+                    </motion.div>
                 </div>    
             <div>
         </div>
